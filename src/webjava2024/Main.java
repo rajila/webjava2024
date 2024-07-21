ce748c22f93250874a51d7f157d587fd81a13ee1
rajila <rdajila@gmail.com>
2024-07-21 04:56:37 +0200

    init

---
+++ src/webjava2024/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package webjava2024;
+
+/**
+ * 
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola desde Java 2024!!");
+	}
+
+}
---
+++ src/webjava2024/poo/Cuenta.java
@@ -0,0 +1,90 @@
+package webjava2024.poo;
+
+public final class Cuenta {
+	private String Nombre;
+	private String NroCuenta;
+	private double Interes;
+	private double Saldo;
+	
+	public Cuenta() {}
+	
+	public Cuenta(String nombre, String nroCuenta, double interes, double saldo) {
+		this.Nombre = nombre;
+		this.NroCuenta = nroCuenta;
+		this.Interes = interes;
+		this.Saldo = saldo;
+	}
+	
+	public Cuenta (final Cuenta data) {
+		this.Nombre = data.Nombre;
+		this.NroCuenta = data.NroCuenta;
+		this.Interes = data.Interes;
+		this.Saldo = data.Saldo;
+	}
+	
+	/**
+	 * Se realiza un ingreso a la cuenta
+	 * @param importe
+	 * @return
+	 */
+	public boolean Ingreso (double importe) {
+		if (importe > 0) {
+			this.Saldo += importe;
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Se descuenta el importe de la cuenta
+	 * @param importe
+	 * @return
+	 */
+	public boolean Reintegro (double importe) {
+		if (this.Saldo >= importe && importe > 0 ) {
+			this.Saldo -= importe;
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean Transferencia (Cuenta cuentaDestino, double importe) {
+		if (this.Reintegro(importe)) {
+			cuentaDestino.Saldo += importe;
+			return true;
+		}
+		return false;
+	}
+	
+	public String getNombre() {
+		return Nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	
+	public String getNroCuenta() {
+		return NroCuenta;
+	}
+	
+	public void setNroCuenta(String nroCuenta) {
+		NroCuenta = nroCuenta;
+	}
+	
+	public double getInteres() {
+		return Interes;
+	}
+	
+	public void setInteres(double interes) {
+		Interes = interes;
+	}
+	
+	public double getSaldo() {
+		return Saldo;
+	}
+	
+	public void setSaldo(double saldo) {
+		Saldo = saldo;
+	}
+}
