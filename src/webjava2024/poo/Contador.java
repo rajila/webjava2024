eaa8708412074a50bc62375f169beee95babf017
rajila <rdajila@gmail.com>
2024-07-23 17:19:49 +0200

    changes: class contador poo

---
+++ src/webjava2024/poo/Contador.java
@@ -0,0 +1,34 @@
+package webjava2024.poo;
+
+public final class Contador {
+	private int Cont;
+	
+	public Contador(){}
+	
+	public Contador(int cont) {
+		if (cont > 0) this.Cont = cont;
+		else this.Cont = 0;
+	}
+	
+	public Contador (final Contador contadorSource) {
+		this.Cont = contadorSource.Cont;
+	}
+
+	public int getCont() {
+		return Cont;
+	}
+
+	public void setCont(int cont) {
+		Cont = cont;
+	}
+	
+	public void incrementar() {
+		this.Cont++;
+	}
+	
+	public void decrementar() {
+		this.Cont--;
+		if (this.Cont < 0 ) this.Cont = 0;
+	}
+
+}
